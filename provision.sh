9755376cbf5f6bb94b7f7e30fb133f291c59bcaf
Toshiaki Maki <tmaki@pivotal.io>
2020-06-30 11:36:04 +0900

    Add docker installation

--- provision.sh
+++ provision.sh
@@ -154,3 +154,48 @@ export PATH=\${PATH}:\${MAVEN_HOME}/bin
 EOF
   touch ${PROVISION}/maven
 fi
+
+if [ ! -f ${PROVISION}/docker-repo ];then
+  echo "==== Setup docker repo ===="
+  sudo apt-get remove -y docker docker-engine docker.io containerd runc
+  sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update -y
+  touch ${PROVISION}/docker-repo
+fi
+
+if [ ! -f ${PROVISION}/docker ];then
+  DOCKER_VERSION=5:19.03.8~3-0~ubuntu-bionic
+  CONTAINERD_VERSION=1.2.13-1
+  # install docker
+  sudo apt-get remove -y docker docker-engine docker.io containerd runc
+  sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+  sudo apt-get update -y
+  sudo apt-get install -y docker-ce=${DOCKER_VERSION} docker-ce-cli=${DOCKER_VERSION} containerd.io=${CONTAINERD_VERSION}
+  sudo systemctl enable docker && sudo systemctl start docker
+  sudo usermod -aG docker ubuntu
+  sudo apt-mark hold docker-ce docker-ce-cli containerd.io
+
+  sudo mkdir -p /etc/docker
+  cat <<'EOF' | sudo tee -a /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2",
+  "storage-opts": [
+    "overlay2.override_kernel_check=true"
+  ]
+}
+EOF
+
+  # Restart Docker
+  sudo systemctl daemon-reload
+  sudo systemctl restart docker
+  touch ${PROVISION}/docker
+fi
